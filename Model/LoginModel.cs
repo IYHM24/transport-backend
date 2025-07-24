58f95d129080b313ac390e6178f0de65391b67d2
Andrés C. Gutiérrez <dev.andres.gutierrez@gmail.com>
2025-07-23 19:17:57 -0500

    [fixed] - problema con dependencias

--- Model/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model
-{
-    public class LoginModel
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- Model/RoleModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model
-{
-    public  class RoleModel
-    {
-        public required string RoleName { get; set; }
-        public required string CodEmpresa { get; set; }
-    }
-}
